1fab4938a01f93dd3730f491f04c9c74d8cbe1f4
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-04-18 23:14:49 +0200

    Rendering Data from our Database in a View

--- trydjango/views.py
+++ trydjango/views.py
@@ -3,6 +3,7 @@ To render html web pages
 """
 import random
 from django.http import HttpResponse
+from articles.models import Article
 
 
 def home_view(request):
@@ -10,15 +11,26 @@ def home_view(request):
     Take in a request (Django sends request)
     Return HTML as a response (We pick to return the response)
     """
+    # first method to create data in model
+    # Remove comment below to create data in model
+    # obj = Article(title='Hello World', content='This is awesome')
+    # obj.save()
+    # second method to create data in model
+    # Remove comment below to create data in model
+    # Article.objects.create(title='Hello World Again', content='This is awesome')
+
     name = "Shabayek"
-    number = random.randint(10, 1233123)
+    random_id = random.randint(1, 4) # pseudo random
+
+    # from the database??
+    article_obj = Article.objects.get(id=random_id)
 
     # Django Templates
     H1_STRING = f"""
-    <h1>Hello {name} - {number}!</h1>
+    <h1>{article_obj.title} (id: {article_obj.id})!</h1>
     """
     P_STRING = f"""
-    <p>Hi {name} - {number}!</p>
+    <p>{article_obj.content}!</p>
     """
     HTML_STRING = H1_STRING + P_STRING
     return HttpResponse(HTML_STRING)
