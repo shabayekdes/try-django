376ee3f38fd5dbebe49508106ed64d83887d1587
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-04-19 00:54:10 +0200

    Add view article with dynamic url

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from .models import Article
+
 # Create your views here.
+def view_article(request, id=None):
+    article = None
+    if id is not None:
+        article = Article.objects.get(id=id)
+        
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/view.html', context=context)
