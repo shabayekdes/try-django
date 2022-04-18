3f678dfd103f9983a05fb08d7423894b08630118
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-04-19 00:20:16 +0200

    Add django template basics

--- trydjango/views.py
+++ trydjango/views.py
@@ -3,6 +3,7 @@ To render html web pages
 """
 import random
 from django.http import HttpResponse
+from django.template.loader import render_to_string, get_template
 from articles.models import Article
 
 
@@ -12,7 +13,7 @@ def home_view(request):
     Return HTML as a response (We pick to return the response)
     """
     # first method to create data in model
-    # Remove comment below to create data in model
+    # TODO: remove comment below to create data in model
     # obj = Article(title='Hello World', content='This is awesome')
     # obj.save()
     # second method to create data in model
@@ -25,12 +26,21 @@ def home_view(request):
     # from the database??
     article_obj = Article.objects.get(id=random_id)
 
+    # context will be passed to view
+    context = {
+        'id': article_obj.id,
+        'title': article_obj.title,
+        'content': article_obj.content,
+    }
+
     # Django Templates
-    H1_STRING = f"""
-    <h1>{article_obj.title} (id: {article_obj.id})!</h1>
-    """
-    P_STRING = f"""
-    <p>{article_obj.content}!</p>
-    """
-    HTML_STRING = H1_STRING + P_STRING
+    # First method using get_template
+    # TODO: remove comment below to use get_template
+    # temp = get_template('home_view.html')
+    # HTML_STRING = temp.render(context=context)
+
+    # Second method using render_to_string
+    # TODO: remove comment below to use render_to_string
+    HTML_STRING = render_to_string('home_view.html', context=context)
+
     return HttpResponse(HTML_STRING)
