54cbacc1ab7002202701f6962ae8fc39f07dd4d7
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-04-20 22:37:51 +0200

    Enhance login with built in authentication form in django

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
@@ -14,15 +14,17 @@ def register_view(request):
 
 def login_view(request):
     if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(request, username=username, password=password)
-        if user is None:
-            context = {"error": "Invalid username or password."}
-            return render(request, "accounts/login.html", context)
-        login(request, user)
-        return redirect('/')
-    return render(request, "accounts/login.html", {})
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, "accounts/login.html", context=context)
 
 def logout_view(request):
     if request.method == "POST":
