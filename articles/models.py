954cab105e67c9fac1ce9bf0eaef73cf79957718
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-04-22 22:37:41 +0200

    Add article test case

--- articles/models.py
+++ articles/models.py
@@ -20,14 +20,12 @@ class Article(models.Model):
         super().save(*args, **kwargs)
 
 def article_pre_save(sender, instance, *args, **kwargs):
-    print('pre_save')
     if instance.slug is None:
         slugify_instance_title(instance, save=False)
 
 pre_save.connect(article_pre_save, sender=Article)
 
 def article_post_save(sender, instance, created, *args, **kwargs):
-    print('post_save')
     if created:
         slugify_instance_title(instance, save=True)
 
