66de98821cffce1737524c0f454202afd864d49b
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-04-22 22:24:23 +0200

    Auto generate slugs with recursion

--- articles/models.py
+++ articles/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.utils import timezone
-from django.utils.text import slugify
 from django.db.models.signals import pre_save, post_save
 
+from .utils import slugify_instance_title
 # Create your models here.
 class Article(models.Model):
     # https://docs.djangoproject.com/en/3.2/ref/models/fields/#model-field-types
     # Django model-field-types
     title = models.CharField(max_length=120)
     content = models.TextField()
-    slug = models.SlugField(blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     published_at = models.DateField(auto_now_add=False, auto_now=False, null=True, blank=True)
@@ -18,16 +18,6 @@ class Article(models.Model):
         # if self.slug is None:
         #     self.slug = slugify(self.title)
         super().save(*args, **kwargs)
-
-def slugify_instance_title(instance, save=False):
-    slug = slugify(instance.title)
-    qs = Article.objects.filter(slug=slug).exclude(id=instance.id)
-    if qs.exists():
-        slug = f"{slug}-{qs.count() + 1}"
-    instance.slug = slug
-    if save:
-        instance.save()
-    return instance
 
 def article_pre_save(sender, instance, *args, **kwargs):
     print('pre_save')
