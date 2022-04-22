2553d36049225a43c77c577e5f485c2997e2f6ca
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-04-22 21:23:15 +0200

    SlugField & override save method

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 
 # Create your models here.
 class Article(models.Model):
@@ -7,6 +8,12 @@ class Article(models.Model):
     # Django model-field-types
     title = models.CharField(max_length=120)
     content = models.TextField()
+    slug = models.SlugField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     published_at = models.DateField(auto_now_add=False, auto_now=False, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.slug is None:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
