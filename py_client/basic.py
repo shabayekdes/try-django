4231ce14f89aaaf4648a55d5fc7ff9a684588410
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-05-13 22:29:37 +0200

    Django Model Instance to Dictionary

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,13 +3,12 @@ import requests
 endpoint = "http://localhost:8000/api/"
 
 get_response = requests.get(endpoint, params={'page': 1}, json={"product_id": 123 }) # HTTP Request
-# print raw text response
-# print(get_response.text) 
+# print(get_response.headers)
+# print(get_response.text) # print raw text response
+# print(get_response.status_code)
 
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON
 # JavaScript Object Notation ~ Python Dict
-# print(get_response.json())
-print(get_response.status_code)
 
 print(get_response.json())
