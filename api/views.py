692d767db05e2947fed850c0b1dd516a5a4b5a19
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-05-14 13:28:53 +0200

    Rest Framework View & Response

--- api/views.py
+++ api/views.py
@@ -1,11 +1,16 @@
-from django.http import JsonResponse
-import json
 from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from products.models import Product
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    """
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
         data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-    return JsonResponse(data)
+    return Response(data)
