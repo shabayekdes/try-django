db0f37316d996d49bdbb7851e06bc650505accf8
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-05-16 22:13:33 +0200

    Custom Validation with Serializers

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
-from .models import Product
 
+from .models import Product
+from . import validators
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
@@ -10,6 +11,8 @@ class ProductSerializer(serializers.ModelSerializer):
             view_name='product-detail',
             lookup_field='pk'
     )
+    title = serializers.CharField(validators=[validators.validate_title_no_hello, validators.unique_product_title])
+    # name = serializers.CharField(source='title', read_only=True)
     class Meta:
         model = Product
         fields = [
@@ -17,11 +20,31 @@ class ProductSerializer(serializers.ModelSerializer):
             'edit_url',
             'pk',
             'title',
+            # 'name',
             'content',
             'price',
             'sale_price',
             'my_discount',
         ]
+    
+    # def validate_title(self, value):
+    #     request = self.context.get('request')
+    #     user = request.user
+    #     qs = Product.objects.filter(user=user, title__iexact=value)
+    #     if qs.exists():
+    #         raise serializers.ValidationError(f"{value} is already a product name.")
+    #     return value
+    
+    # def create(self, validated_data):
+    #     # return Product.obejcts.create(**validated_data)
+    #     # email = validated_data.pop('email')
+    #     obj = super().create(validated_data)
+    #     # print(email, obj)
+    #     return obj
+    
+    # def update(self, instance, validated_data):
+    #     email = validated_data.pop('email')
+    #     return super().update(instance, validated_data)
     
     def get_edit_url(self, obj):
         request = self.context.get('request') # self.request
