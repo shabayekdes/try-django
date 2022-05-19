ff013c357e4f4236ae811ee9043d3d7ec0e52ff2
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-05-19 21:42:06 +0200

    JSON WEB Token Authentication with simplejwt

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
     'django_htmx',
     'storages',
     'rest_framework',
+    'rest_framework_simplejwt',
     'rest_framework.authtoken',
     # internal
     'articles',
@@ -184,7 +186,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
-        "api.authentication.TokenAuthentication"
+        "api.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
@@ -195,6 +198,13 @@ REST_FRAMEWORK = {
 
 ALGOLIA = {
     'APPLICATION_ID': '4IHLYNCMBJ',
-    'API_KEY': 'c614f57ba6a38299bbd901da6fd0b84d',
+    'API_KEY': '4b063049ede11d6f623e7cf47ef6c336',
     'INDEX_PREFIX': 'cfe'
+}
+
+
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": ["Bearer"],
+    "ACCESS_TOKEN_LIFETIME": datetime.timedelta(seconds=30), # minutes=5
+    "REFRESH_TOKEN_LIFETIME": datetime.timedelta(minutes=1), # days=1
 }
